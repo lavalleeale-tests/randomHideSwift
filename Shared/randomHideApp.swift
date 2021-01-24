5b3ed15fae12fb6bf93a66d8a22eb8ee9312a623
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-01-24 13:14:54 -0800

    Initial Commit

---
+++ Shared/randomHideApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomHideApp.swift
+//  Shared
+//
+//  Created by Alex lavallee on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct randomHideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
